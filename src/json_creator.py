a1c8c65a177916c9284c4c64f49edf776d06ae36
Michelle <michawest@bk.ru>
2024-10-15 16:43:34 +0300

    added max and min field

--- src/json_creator.py
+++ src/json_creator.py
@@ -8,8 +8,10 @@ class JSON_node:
 
     def __init__(self, name: str = '', is_root: bool = False, doc: str = '', param: list[{str: str}] = []):
         self.class_name: str = name
-        self.isRoot: bool = is_root
         self.documentation: str = doc
+        self.isRoot: bool = is_root
+        self.max = None
+        self.min = None
         self.parameters: list[{str: str}] = param
 
     def add_param(self, param_name: str, param_type: str):
@@ -19,6 +21,10 @@ class JSON_node:
         json_dict = {'class': self.class_name}
         json_dict.update(self.__dict__.copy())
         del json_dict['class_name']
+        if self.max is None:
+            del json_dict['max']
+        if self.min is None:
+            del json_dict['min']
         return json_dict
 
     def __eq__(self, other):
@@ -35,6 +41,14 @@ class JSON_node:
 
     def __str__(self):
         return self.class_name+" "+str(self.isRoot)+" "+self.documentation+" "+str(self.parameters)
+
+
+def get_max_min_sourceMultiplicity(input_str: str):
+    res = input_str.split("..")
+    if len(res) == 2:
+        return int(res[0]), int(res[1])
+    else:
+        return int(res[0]), int(res[0])
 
 
 class JSON_creator:
@@ -90,6 +104,12 @@ class JSON_creator:
             attr_name = t.attrs['name']
             attr_type = t.attrs['type']
             node.parameters.append({'name': attr_name, 'type': attr_type})
+
+        aggregation_tags = self.BS_data.findAll('Aggregation', {'source': tag_name})
+        for t in aggregation_tags:
+            min_val, max_val = get_max_min_sourceMultiplicity(t.attrs['sourceMultiplicity'])
+            node.max = max_val
+            node.min = min_val
 
         aggregation_tags = self.BS_data.findAll('Aggregation', {'target': tag_name})
         for t in aggregation_tags:
