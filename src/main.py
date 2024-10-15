4052731c70ab92a954742b2388417ddbf6a367a5
Michelle <michawest@bk.ru>
2024-10-15 16:09:10 +0300

    took into account the changes in xml_ and json_ creator

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 
 from json_creator import JSON_creator
-from xml_creator import XML_creator, XML_node
+from xml_creator import XML_creator
 
 input_path = "input/test_input.xml"
 with open(input_path, 'r') as f:
     BS_data = BeautifulSoup(f, features="xml")
-    creator_xml = XML_creator(BS_data, "out/output.xml")
-    creator_json = JSON_creator(BS_data, "out/output.json")
+    creator_xml = XML_creator(BS_data, "out/config.xml")
+    creator_json = JSON_creator(BS_data, "out/meta.json")
 
     creator_xml.create_file()
     creator_json.create_file()
